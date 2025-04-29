76e48bafd5cee5d52204eb539fe09b5e3f3e9936
kellvyno <165435790+kellvyno@users.noreply.github.com>
2025-04-28 23:31:58 -0300

    Add files via upload

---
+++ 123.js
@@ -0,0 +1,11 @@
+echo "# 3-" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/kellvyno/3-.git
+git push -u origin main
+
+git remote add origin https://github.com/kellvyno/3-.git
+git branch -M main
+git push -u origin main
